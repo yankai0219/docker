5b7ad88f6e5dd27584fb8736613687b23f5127da
keything <keything@keythingdeMacBook-Pro.local>
2016-02-23 11:46:51 +0800

    modify something

--- Dockerfile.php
+++ Dockerfile.php
@@ -6,11 +6,10 @@ FROM yk/centos_nginx
 MAINTAINER yankai-c <yankai-c@360.cn>
 
 ## basic enviroment
-RUN yum -y install mcrypt mhash libmcrypt libmcrypt-devel
 ## install php
-RUN wget http://172.30.65.9:82/1Q2W3E4R5T6Y7U8I9O0P1Z2X3C4V5B/cz2.php.net/distributions/php-7.0.3.tar.gz -O /tmp/php-7.0.3.tar.gz; \
-    tar zxvf /tmp/php-7.0.3.tar.gz -C /tmp/; \
-    cd /tmp/php-7.0.3; \
+RUN wget http://cn2.php.net/distributions/php-5.6.18.tar.gz -O /tmp/php-5.6.18.tar.gz; \
+    tar zxvf /tmp/php-5.6.18.tar.gz -C /tmp/; \
+    cd /tmp/php-5.6.18; \
     ./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd -with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir=/usr --enable-xml --disable-rpath --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --enable-mbregex --enable-mbstring --with-mcrypt --enable-ftp --with-gd --enable-gd-native-ttf --with-openssl --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc --enable-zip --enable-soap --with-gettext --disable-fileinfo --enable-opcache --enable-fpm; \
     make && make install
 
